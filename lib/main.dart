609b40e31ecaeaf381b19616cd2a8c2aa637b829
Asem <asemabdo2131999@gmail.com>
2025-10-20 21:34:47 +0300

    First commit.

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/theme/my_theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      theme: MyTheme.theme,
+      debugShowCheckedModeBanner: false,
+      home: Container(),
+    );
+  }
+}
