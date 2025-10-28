df063ecb1bf3e2890922217d6d251e1e8c14a226
Asem <asemabdo2131999@gmail.com>
2025-10-28 15:39:28 +0300

    Change class MyTheme to be final myTheme = ..

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
-      theme: MyTheme.theme,
+      theme: myTheme,
       debugShowCheckedModeBanner: false,
       home: const TabsScreen(),
     );
